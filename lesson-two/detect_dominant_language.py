622700db8d75ce351ca18f00ce60b65b23c9d69f
jeffreyjackovich <jeffrey.jackovich@gmail.com>
2018-06-26 15:16:23 -0400

    lesson2 detectDominantLang added

---
+++ lesson-two/detect_dominant_language.py
@@ -0,0 +1,9 @@
+import boto3
+import json
+
+comprehend = boto3.client(service_name='comprehend', region_name='<input region>')
+text = "Machine Learning is fascinating."
+
+print('Calling DetectDominantLanguage')
+print(json.dumps(comprehend.detect_dominant_language(Text = text), sort_keys=True, indent=4))
+print("End of DetectDominantLanguage\n")
