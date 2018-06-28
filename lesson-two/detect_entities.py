6449e0362bb2cabf22e7277794c4be7978c94273
jeffreyjackovich <jeffrey.jackovich@gmail.com>
2018-06-28 15:24:32 -0400

    files updated

--- lesson-two/detect_entities.py
+++ lesson-two/detect_entities.py
@@ -8,14 +8,8 @@ comprehend = boto3.client(service_name='comprehend', region_name='<input region>
 
 # provide english and spanish text to analyze
 english_text = 'Machine Learning is fascinating.'
-spanish_text = 'El aprendizaje automático es fascinante.'
 
-print('Calling DetectDominantLanguage')
-
-print('english_text result:')
+print('Calling DetectEntities')
 # json.dumps() writes JSON data to a Python string
-print(json.dumps(comprehend.detect_dominant_language(Text = english_text), sort_keys=True, indent=4))
-
-print('\nspanish_text result:')
-print(json.dumps(comprehend.detect_dominant_language(Text = spanish_text), sort_keys=True, indent=4))
-print('End of DetectDominantLanguage\n')
+print(json.dumps(comprehend.detect_entities(Text = english_text), sort_keys=True, indent=4))
+print('End of DetectEntities\n')
