c807ea33b5305ec371baf7f8512a2ce90567a663
jeffreyjackovich <jeffrey.jackovich@gmail.com>
2018-06-27 22:32:08 -0400

    added document links for boto3 and json

--- lesson-two/detect_dominant_language.py
+++ lesson-two/detect_dominant_language.py
@@ -1,15 +1,14 @@
-# import the AWS SDK for python (boto3)
+# import the AWS SDK for python (boto3) - http://boto3.readthedocs.io/en/latest/
 import boto3
-# import json module to serialize JSON 
+# import json module to serialize JSON - https://docs.python.org/3.6/library/json.html
 import json
 
 # instantiate a new comprehend client
 comprehend = boto3.client(service_name='comprehend', region_name='<input region>')
 
 # provide english and spanish text to analyze
-english_text = "Machine Learning is fascinating."
-spanish_text = "El aprendizaje automático es fascinante."
-
+english_text = 'Machine Learning is fascinating.'
+spanish_text = 'El aprendizaje automático es fascinante.'
 
 print('Calling DetectDominantLanguage')
 
@@ -19,5 +18,4 @@ print(json.dumps(comprehend.detect_dominant_language(Text = english_text), sort_
 
 print('\nspanish_text result:')
 print(json.dumps(comprehend.detect_dominant_language(Text = spanish_text), sort_keys=True, indent=4))
-
-print("End of DetectDominantLanguage\n")
+print('End of DetectDominantLanguage\n')
