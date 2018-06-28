85a2580798440a9f06b5a46cf64e53d30f39c404
jeffreyjackovich <jeffrey.jackovich@gmail.com>
2018-06-28 15:53:55 -0400

    fixed batch output

--- lesson-two/batch_detect_dominant_languages.py
+++ lesson-two/batch_detect_dominant_languages.py
@@ -10,7 +10,7 @@ comprehend = boto3.client(service_name='comprehend', region_name='<input region>
 english_string_list = ['Machine Learning is fascinating.', 'Studying Artificial Intelligence is my passion.'] 
 spanish_string_list = ['El aprendizaje automático es fascinante.', 'Estudiar Inteligencia Artificial es mi pasión.']
 
-print('Calling DetectDominantLanguage')
+print('Calling BatchDetectDominantLanguage')
 
 print('english_string_list result:')
 # json.dumps() writes JSON data to a Python string
@@ -18,4 +18,4 @@ print(json.dumps(comprehend.batch_detect_dominant_language(TextList = english_st
 
 print('\nspanish_string_list result:')
 print(json.dumps(comprehend.batch_detect_dominant_language(TextList = spanish_string_list), sort_keys=True, indent=4))
-print('End of DetectDominantLanguage\n')
+print('End of BatchDetectDominantLanguage\n')
